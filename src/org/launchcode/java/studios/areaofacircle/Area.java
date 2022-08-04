2b86cfe95d16b750278e5173f547937a758c0351
Kim Lee <you@example.com>
2022-08-03 20:42:44 -0500

    completed area of a circle studio

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,20 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter radius: ");
+        double radius = input.nextDouble();
+        String radiusStr = input.nextLine();
+        do {
+            System.out.println("Enter a valid number!");
+            System.out.println("Enter radius: ");
+            radius = input.nextDouble();
+        } while ((radius <= 0) || !input.hasNextDouble());
+            double area = Circle.getArea(radius);
+            System.out.println("The area of a circle of radius " + radius + " is " + area);
+            input.close();
+    }
+}
